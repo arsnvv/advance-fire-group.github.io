6747fe78bcd722a9152922c327b3c437cc2d7be9
arsnvv <62116239+arsnvv@users.noreply.github.com>
2020-03-18 23:39:06 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ menu.onclick = function myFunction() {
 		x.className = "topnav";
 	}
 }
+
+
+$('body').scrollspy({target: ".topnav", offset: 50});
+$("#myTopnav a").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      window.location.hash = hash;
+    });
+  }
+});
