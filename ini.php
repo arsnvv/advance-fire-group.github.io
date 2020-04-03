d847f754ed590aaffab3ecc86a8e425babb21fea
arsnvv <62116239+arsnvv@users.noreply.github.com>
2020-04-03 21:57:30 +0300

    Delete ini.php

--- ini.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db_name="135";
-$db_user="root";
-$db_pass="";
-$my_db=mysqli_connect('localhost',$db_user,$db_pass,$db_name) or die("не работает");
-mysqli_query($my_db,"SET NAMES 'utf8' ");
-?>
